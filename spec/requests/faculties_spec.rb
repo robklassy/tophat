41f9ea701ed3ec75d2134caae7ead53238e28dd7
Rob K <kalsikam@gmail.com>
2024-07-22 00:15:47 -0400

    added faculties spec

---
+++ spec/requests/faculties_spec.rb
@@ -0,0 +1,56 @@
+require 'swagger_helper'
+require 'rake'
+Rails.application.load_tasks
+
+RSpec.describe 'faculties', type: :request do
+  before(:all) do
+    Rake::Task["db:seed"].invoke
+  end
+
+  path '/faculties' do
+
+    get('list faculties') do
+      response(200, 'successful') do
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test! do |response|
+          db_ids = Faculty.all.map(&:id).sort
+          expect(db_ids.count).to be > 0
+          data = JSON.parse(response.body)
+          res_ids = data.collect { |s| s['id'] }
+          expect(db_ids).to eq(res_ids.sort)
+        end
+      end
+    end
+  end
+
+  path '/faculties/{id}' do
+    parameter name: 'id', in: :path, type: :string, description: 'id'
+
+    get('show faculty') do
+      response(200, 'successful') do
+        let(:id) { Faculty.all.first.id }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test! do |response|
+          data = JSON.parse(response.body)
+          expect(id).not_to eq(nil)
+          expect(data['id']).not_to eq(nil)
+          expect(data['id']).to eq(id)
+        end
+      end
+    end
+  end
+end
